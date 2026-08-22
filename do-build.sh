2fbbd5af66f5ebb4ba9ee42112ba60c973b426c6
samwhelp <samwhelp@users.noreply.github.com>
2026-08-22 14:34:29 +0800

    fix

--- do-build.sh
+++ do-build.sh
@@ -1868,6 +1868,11 @@ function model_do_fulfill_scripts () {
 	extend_master_file_install
 
 
+	#base_dconf_package_install
+	#base_dconf_db_update
+	#base_gsettings_schema_compile
+
+
 	extend_installer_package_install
 	extend_installer_file_install
 
